96a682930987d1f1a1eb7aea680e856cd1c51c5f
Marvin Zoora <w0870466@myscc.ca>
2026-03-06 09:43:32 -0500

    Assignment 6 completed

--- app/src/main/java/com/example/habit_tracker_marvin_zoora/MainActivity.kt
+++ app/src/main/java/com/example/habit_tracker_marvin_zoora/MainActivity.kt
@@ -3,45 +3,146 @@ package com.example.habit_tracker_marvin_zoora
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.habit_tracker_marvin_zoora.ui.theme.HabitTrackerMarvinZooraTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//i use a data class for a habit and boolean whether it's completed
+data class Habit(
+    val name: String,
+    var completed: Boolean = false
+)
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            HabitTrackerMarvinZooraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HabitTrackerApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HabitTrackerApp() {
+    //text entered by user
+    var habitText by remember { mutableStateOf("") }
+    // list to store habits
+    val habitList = remember {
+        mutableStateListOf<Habit>()
+    }
 
-@Preview(showBackground = true)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+
+        Text(
+            text = "Student Habit Tracker",
+            fontSize = 26.sp
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        //input section to add a habit
+        HabitInputSection(
+            habitText = habitText,
+            onTextChange = { habitText = it },
+            onAddClick = {
+                if (habitText.isNotBlank()) {
+                    habitList.add(Habit(habitText))
+                    habitText = ""
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+            //display list of habits
+        HabitList(
+            habits = habitList
+        )
+    }
+}
+//composable for habit input section
 @Composable
-fun GreetingPreview() {
-    HabitTrackerMarvinZooraTheme {
-        Greeting("Android")
+fun HabitInputSection(
+    habitText: String,
+    onTextChange: (String) -> Unit,
+    onAddClick: () -> Unit
+) {
+
+    Column {
+
+        Text("Enter a new habit")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = habitText,
+            onValueChange = onTextChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = onAddClick) {
+            Text("Add Habit")
+        }
+    }
+}
+//dispaly list of habits
+@Composable
+fun HabitList(
+    habits: MutableList<Habit>
+) {
+
+    LazyColumn {
+
+        itemsIndexed(habits) { index, habit ->
+
+            HabitItem(
+                habit = habit,
+                onCompletedClick = {
+                    habit.completed = !habit.completed
+                }
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+//composable to display single habit row
+@Composable
+fun HabitItem(
+    habit: Habit,
+    onCompletedClick: () -> Unit
+) {
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Text(
+            text = habit.name,
+            color = if (habit.completed) Color.Gray else Color.Black,
+            textDecoration = if (habit.completed)
+                TextDecoration.LineThrough
+            else
+                TextDecoration.None
+        )
+
+        Button(onClick = onCompletedClick) {
+            Text("Completed")
+        }
     }
 }
